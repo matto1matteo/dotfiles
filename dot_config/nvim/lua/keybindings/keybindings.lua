f6e8d91880f7f90f2f27ed71e8e486b24d36f8c1
Matteo Maria Emanuele Cialei <matteome.cialei@gmail.com>
2022-04-07 11:01:55 +0200

    some better keybindings

--- dot_config/nvim/lua/keybindings/keybindings.lua
+++ dot_config/nvim/lua/keybindings/keybindings.lua
@@ -3,18 +3,18 @@ local map = require("keybindings").map
 -- Disable higlith after search by clicking <enter>
 -- Had to remap couse <CR> was already mapped to next line
 -- nnoremap <CR> :noh<CR>
-map("n", "<CR>", ":noh<CR>", {noremap = true})
+map("n", "<CR>", "<cmd>noh<CR>", {noremap = true})
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "_"
 
-map("n", "<C-p>", ":FZF<CR>", {noremap = true})
+map("n", "<C-p>", "<cmd>Telescope find_files<CR>", {noremap = true})
 
 -- " Markdown remap
 map("n", "<leader>P", "<Plug>MarkdownPreview")
-map("n", "<leader>F", ":Format<CR>", {silent = true, noremap = true})
+map("n", "<leader>F", "<cmd>Format<CR>", {silent = true, noremap = true})
 
 -- " Tab remap
-map("n", "tn", ":tabnew<CR>")
+map("n", "tn", "<cmd>tabnew<CR>")
 map("n", "pt", "gT", {noremap = true})
 map("n", "nt", "gt", {noremap = true})
