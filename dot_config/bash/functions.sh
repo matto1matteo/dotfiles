ba0e25c0c686ba6499518ab706530b066db937eb
Matteo Maria Emanuele Cialei <matteome.cialei@gmail.com>
2025-12-29 12:47:45 +0100

    i3, bash

--- dot_config/bash/functions.sh
+++ dot_config/bash/functions.sh
@@ -1,3 +1,20 @@
 cvim() {
     chezmoi edit $1 --apply
 }
+
+getBranch() {
+    if [[ -d "$(pwd)/.git" ]]
+    then
+        echo "[$(git branch | sed -n '1{s/^* *//;p}')] "
+    fi
+}
+
+lastdir() {
+    var=($(echo $(pwd) | tr '/' '\n'))
+    echo ${var[-1]}
+}
+
+tmup() {
+    tmux new -s $(lastdir) -n editor nvim . \; neww -n term \; neww -n misc \; select-window -t editor
+}
+
